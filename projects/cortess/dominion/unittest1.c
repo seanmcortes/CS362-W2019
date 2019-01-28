ff6ffcfb9b5a9c8483fe3c2f07cb0e11ba69e3e4
Sean Cortes <cortess@flip1.engr.oregonstate.edu>
2019-01-28 14:42:49 -0800

    add unittest1

---
+++ projects/cortess/dominion/unittest1.c
@@ -0,0 +1,12 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+
+int main() {
+	printf("hello world");
+	return 0;
+}
